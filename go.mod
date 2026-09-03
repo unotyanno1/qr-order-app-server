fbb18a4165ea58e9187a555c1cb932356cfde6e3
oura_kazuhiro <unotyanno1@gmail.com>
2026-09-04 00:45:16 +0900

    DB接続処理 タイムアウト処理追加　インデント修正

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/unotyanno1/qr-order-app-server
 go 1.24.0
 
 require (
-	github.com/go-sql-driver/mysql v1.9.3
+	github.com/go-sql-driver/mysql v1.10.1
 	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/labstack/echo/v4 v4.15.0
 )
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
+	filippo.io/edwards25519 v1.2.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- internal/infra/database/mysql.go
+++ internal/infra/database/mysql.go
@@ -1,22 +1,22 @@
 package database
 
 import (
-    "database/sql"
-    "os"
-    "fmt"
+	"database/sql"
+	"fmt"
+	"os"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewMySQL() (*sql.DB, error) {
-    user := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASSWORD")
-    host := os.Getenv("DB_HOST")
-    port := os.Getenv("DB_PORT")
-    dbName := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-    dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&timeout=5s",
 		user,
 		password,
 		host,
@@ -24,15 +24,18 @@ func NewMySQL() (*sql.DB, error) {
 		dbName,
 	)
 
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        return nil, fmt.Errorf("faild to open database: %w", err)
-    }
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
 
-    if err := db.Ping(); err != nil {
-        db.Close()
-        return nil, fmt.Errorf("failed to connect database: %w", err)
-    }
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-    return db, nil
+	if err := db.PingContext(ctx); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	return db, nil
 }
